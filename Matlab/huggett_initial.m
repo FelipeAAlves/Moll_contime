33ae2138f575298dc417a6c03709c49b1435e5eb
FelipeAAlves <felipe.a.alves0@gmail.com>
2017-07-20 19:24:39 -0400

    work on transition and stste

--- Matlab/huggett_initial.m
+++ Matlab/huggett_initial.m
@@ -44,12 +44,16 @@ r = r0;
 v0(:,1) = (z(1) + r.*a).^(1-s)/(1-s)/rho;
 v0(:,2) = (z(2) + r.*a).^(1-s)/(1-s)/rho;
 
-for ir=1:Ir;
 
-r_r(ir)=r;
-rmin_r(ir)=rmin;
-rmax_r(ir)=rmax;
-    
+R_r = linspace(0.04,-0.025,66)
+
+for ir=1:66;
+
+r = R_r(ir)
+% r_r(ir)=r;
+% rmin_r(ir)=rmin;
+% rmax_r(ir)=rmax;
+
 if ir>1
 v0 = V_r(:,:,ir-1);
 end
@@ -65,9 +69,9 @@ for n=1:maxit
     % backward difference
     dVb(2:I,:) = (V(2:I,:)-V(1:I-1,:))/da;
     dVb(1,:) = (z + r.*amin).^(-s); %state constraint boundary condition
-    
+
     I_concave = dVb > dVf; %indicator whether value function is concave (problems arise if this is not the case)
-    
+
     %consumption and savings with forward difference
     cf = dVf.^(-1/s);
     ssf = zz + r.*aa - cf;
@@ -77,9 +81,9 @@ for n=1:maxit
     %consumption and derivative of value function at steady state
     c0 = zz + r.*aa;
     dV0 = c0.^(-s);
-    
+
     % dV_upwind makes a choice of forward or backward differences based on
-    % the sign of the drift    
+    % the sign of the drift
     If = ssf > 0; %positive drift --> forward difference
     Ib = ssb < 0; %negative drift --> backward difference
     I0 = (1-If-Ib); %at steady state
@@ -87,11 +91,11 @@ for n=1:maxit
     %Ib(I,:) = 1; If(I,:) = 0;
     %STATE CONSTRAINT at amin: USE BOUNDARY CONDITION UNLESS muf > 0:
     %already taken care of automatically
-    
+
     dV_Upwind = dVf.*If + dVb.*Ib + dV0.*I0; %important to include third term
     c = dV_Upwind.^(-1/s);
     u = c.^(1-s)/(1-s);
-    
+
     %CONSTRUCT MATRIX
     X = - min(ssb,0)/da;
     Y = - max(ssf,0)/da + min(ssb,0)/da;
@@ -102,15 +106,15 @@ for n=1:maxit
     A = [A1,sparse(I,I);sparse(I,I),A2] + Aswitch;
 
     B = (1/Delta + rho)*speye(2*I) - A;
-    
+
     u_stacked = [u(:,1);u(:,2)];
     V_stacked = [V(:,1);V(:,2)];
-    
+
     b = u_stacked + V_stacked/Delta;
     V_stacked = B\b; %SOLVE SYSTEM OF EQUATIONS
-    
+
     V = [V_stacked(1:I),V_stacked(I+1:2*I)];
-    
+
     Vchange = V - v;
     v = V;
 
@@ -152,19 +156,19 @@ V_r(:,:,ir) = V;
 S(ir) = g(:,1)'*a*da + g(:,2)'*a*da;
 
 %UPDATE INTEREST RATE
-if S(ir)>crit_S
-    disp('Excess Supply')
-    rmax = r;
-    r = 0.5*(r+rmin);
-elseif S(ir)<-crit_S;
-    disp('Excess Demand')
-    rmin = r;
-    r = 0.5*(r+rmax);
-elseif abs(S(ir))<crit_S;
-    display('Equilibrium Found, Interest rate =')
-    disp(r)
-    break
-end
+% if S(ir)>crit_S
+%     disp('Excess Supply')
+%     rmax = r;
+%     r = 0.5*(r+rmin);
+% elseif S(ir)<-crit_S;
+%     disp('Excess Demand')
+%     rmin = r;
+%     r = 0.5*(r+rmax);
+% elseif abs(S(ir))<crit_S;
+%     display('Equilibrium Found, Interest rate =')
+%     disp(r)
+%     break
+% end
 
 end
 
